213273573e3cb5c436c076c9e84aac13954cef52
LAURENS <80611334+laur-ns@users.noreply.github.com>
2022-02-23 20:19:50 +1300

    Implement searchPlaces function

--- src/index.js
+++ src/index.js
@@ -4,22 +4,29 @@ async function getWeatherData(lat, lon) {
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=11ea4e799d422a52dbd686220ca6b1e9`,
       { mode: 'cors' }
     );
+    if (!response.ok) {
+      throw new Error(
+        `${response.status} ${response.statusText} \n Try a different search string`
+      );
+    }
     return response.json();
   } catch (e) {
-    throw new Error(`Error occurred getting weather data: ${e}`);
+    console.log(e);
+    return null;
   }
 }
 
 async function searchPlaces(searchText) {
   try {
     const response = await fetch(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=pk.eyJ1IjoibGF1ci1ucyIsImEiOiJja3p6M3V2cjUwNmcyM2JtbHQydzA3NzdoIn0.qAES-URoq1Fe63KU3eCMmQ`,
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?limit=5&autocomplete=true&types=country,place,address,region,postcode&access_token=pk.eyJ1IjoibGF1ci1ucyIsImEiOiJja3p6M3V2cjUwNmcyM2JtbHQydzA3NzdoIn0.qAES-URoq1Fe63KU3eCMmQ`,
       { mode: 'cors' }
     );
+    console.log(await response.json());
     return response.json();
   } catch (e) {
-    throw new Error(`Error occurred searching for place: ${e}`);
+    throw new Error('An error occured searching for a place.');
   }
 }
 
-getWeatherData();
+searchPlaces('palmerston');
