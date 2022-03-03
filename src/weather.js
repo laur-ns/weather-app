08dd905aee7c3bc2e84ba79432f229474728dce1
LAURENS <80611334+laur-ns@users.noreply.github.com>
2022-03-03 23:53:55 +1300

    Obtain weather data and hide API key

--- src/weather.js
+++ src/weather.js
@@ -1,10 +1,22 @@
+import getWeatherApiKey from './weather-api-key';
+
 const temperatureUnits = 'celsius';
+const key = getWeatherApiKey();
+
+async function getWeatherData(lon, lat) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${key}`
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
 
 const submitWeatherCoords = function submitCoordsToWeatherAPI(coords) {
   const lonCoords = coords.split(',')[0];
   const latCoords = coords.split(',')[1];
   console.log(lonCoords);
   console.log(latCoords);
+  getWeatherData(lonCoords, latCoords);
 };
 
 export default submitWeatherCoords;
